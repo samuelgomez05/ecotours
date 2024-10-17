c4e7f1f2f9138b8d3cac492ff0c9f44a22c8ae07
Samuel Gomez <gomezapari20@gmail.com>
2024-10-16 23:20:40 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{html,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'Urbanist': ['"Urbanist"', 'sans-serif', 'system-ui'],
+        'CaviarDreams': ['"Caviar Dreams"', 'sans-serif', 'system-ui'],
+      },
+      colors: {
+        'primary': '#008b76',
+        'secondary': '#f2af02',
+        'tertiary': '#636363',
+      },
+    },
+  },
+  plugins: [],
+}
+
